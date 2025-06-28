fd28c9ff56a456dcb7bda96111e2bbcf157593c5
Ron Negrov <m4gapower@gmail.com>
2025-06-28 22:58:10 +0300

    added some stuff

--- installer.sh
+++ installer.sh
@@ -1,77 +1,70 @@
 #!/usr/bin/env bash
 #########################################
-#Developed by: Ron Negrov
-#Purpose: The installer script to install all dependencies and install the application
-#Date: 28/06/2025
-#Version: 0.0.0
-##########################################
-. /etc/os-release
-NULL = /dev/null
-LOGFILE = /var/log/ronstaller.log
+# Developed by: Ron Negrov
+# Purpose: The installer script to install all dependencies and install the application
+# Date: 28/06/2025
+# Version: 0.0.1
+#########################################
+
+source /etc/os-release
+NULL=/dev/null
+LOGFILE="/var/log/ronstaller.log"
 DDIR="./dependencies"
 ADIR="./application"
 
-function main(){
-Install_dependencies
-package_build
-run_container
+log() {
+  echo "[$(date '+%Y-%m-%d %H:%M:%S')] $1" | tee -a "$LOGFILE"
 }
 
-function Install_dependencies() {
-    echo "[INFO] Starting dependency installation" >> "$LOGFILE"
-    case "$ID" in
-        debian|ubuntu|kali)
-            for deb in "$DDIR"/*.deb; do
-                echo "[INFO] Installing $deb" | tee -a "$LOGFILE"
-                if sudo apt install -y "$deb" >> "$LOGFILE" 2>&1; then
-                    echo "[SUCCESS] Installed $deb" >> "$LOGFILE"
-                else
-                    echo "[ERROR] Failed to install $deb" >> "$LOGFILE"
-                fi
-            done
-            ;;
-        *)
-            echo "[ERROR] Unsupported OS: $ID" | tee -a "$LOGFILE"
-            exit 1
-            ;;
-    esac
-    echo "[INFO] Dependency installation complete" >> "$LOGFILE"
+main() {
+  log "Installer started"
+  install_dependencies
+  package_build
+  run_container
+  log "Installer finished"
 }
 
-function package_build() {
-    echo "Building Docker image from $ADIR"
-    docker build -t ronstaller_image "$ADIR" >> "$LOGFILE" 2>&1
-
-    if [[ $? -eq 0 ]]; then
-        echo "[INFO] Docker image built successfully" | tee -a "$LOGFILE"
-    else
-        echo "[ERROR] Docker build failed" | tee -a "$LOGFILE"
-        exit 1
-    fi
+install_dependencies() {
+  case "$ID" in
+    debian|ubuntu|kali)
+      for deb in "$DDIR"/*.deb; do
+        log "Installing $deb"
+        sudo dpkg -i "$deb" 2>&1 | tee -a "$LOGFILE"
+      done
+      ;;
+    *)
+      log "Unsupported OS: $ID"
+      exit 1
+      ;;
+  esac
 }
 
-
-function run_container() {
-    echo "[INFO] Starting container run" >> "$LOGFILE"
-
-    if ! command -v docker &>/dev/null; then
-        echo "[ERROR] Docker is not installed or not in PATH" | tee -a "$LOGFILE"
-        exit 1
-    fi
-
-    if ! docker image ls | grep -q nginx; then
-        echo "[INFO] nginx image not found. Pulling from Docker Hub..." >> "$LOGFILE"
-        if ! docker pull nginx >> "$LOGFILE" 2>&1; then
-            echo "[ERROR] Failed to pull nginx image" | tee -a "$LOGFILE"
-            exit 1
-        fi
-    fi
-
-    echo "[INFO] Running nginx container..." >> "$LOGFILE"
-    if docker run -d -p 8000:80 nginx >> "$LOGFILE" 2>&1; then
-        echo "[SUCCESS] Nginx container is running on port 8000" >> "$LOGFILE"
-    else
-        echo "[ERROR] Failed to run nginx container" | tee -a "$LOGFILE"
-        exit 1
-    fi
+package_build() {
+  log "Building Docker image"
+  if docker build -t my_app_image "$ADIR" >> "$LOGFILE" 2>&1; then
+    log "Docker image built successfully"
+  else
+    log "Failed to build Docker image"
+    exit 1
+  fi
 }
+
+run_container() {
+  if ! docker image inspect nginx:latest >"$NULL" 2>&1; then
+    log "nginx image not found. Pulling from Docker Hub"
+    if ! docker pull nginx:latest >> "$LOGFILE" 2>&1; then
+      log "Failed to pull nginx image"
+      exit 1
+    fi
+  fi
+
+  docker run -d -p 8000:80 nginx >> "$LOGFILE" 2>&1
+  if [[ $? -eq 0 ]]; then
+    log "nginx container started successfully"
+  else
+    log "Failed to start nginx container"
+    exit 1
+  fi
+}
+
+main "$@"
